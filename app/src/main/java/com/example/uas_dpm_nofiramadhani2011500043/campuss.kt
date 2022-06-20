57c2674bea3fb312a3633bd51b46232ed83d8356
NofiRamadhani <2011500043@mahasiswa,atmaluhur.ac.id>
2022-06-21 01:29:46 +0700

    UAS DPM NOFI RAMADHANI 043

---
+++ app/src/main/java/com/example/uas_dpm_nofiramadhani2011500043/campuss.kt
@@ -0,0 +1,11 @@
+package com.example.uas_dpm_nofiramadhani2011500043
+
+class campuss (
+    var nidn: String,
+    var nmDosen: String,
+    var jabatan: String,
+    var golonganpangkat: String,
+    var pendidikan: String,
+    var keahlian: String,
+    var programstudi: String,
+)
